ba92c9ca0782fddbb6eb76264faefddb7b379937
t1mch0w <timchou.hit@gmail.com>
2015-10-26 20:17:35 -0400

    modify some operators

--- MyEvaluate.java
+++ MyEvaluate.java
@@ -3,32 +3,61 @@ public class MyEvaluate {
 	public MyEvaluate() {
 	}
 
+  public Node CAR(Node n) {
+  	return n.getLeft();
+	}
+
+	public Node CDR(Node n) {
+		return n.getRight();
+	}
+
+	public Node CAAR(Node n) {
+		return n.getLeft().getLeft();
+	}
+
+	public Node CADR(Node n) {
+		return n.getRight().getLeft();
+	}
+
+	public Node CDAR(Node n) {
+		return n.getLeft().getRight();
+	}
+
+	public Node CDDR(Node n) {
+		return n.getRight().getRight();
+	}
+
 	public boolean startEval(Node n) {
 		if (n.getType()==4) {
 			return true;
 		}
 		else if (n.getType()==5) {
-			if (n.getLeft().getType()==4) {
-				Node left = n.getLeft();
-				Node right = n.getRight();
+			if (CAR(n).getType()==4) {
+				Node left = CAR(n);
+				Node right = CDR(n);
 				if ((left.getValue().equals("PLUS")) || (left.getValue().equals("MINUS")) || (left.getValue().equals("TIMES")) || (left.getValue().equals("QUOTIENT")) || (left.getValue().equals("REMAINDER"))) {
-					if ((right.getLeft() == null) || (right.getRight() == null)) {
+					if (!CDDR(CDR(n)).getValue().equals("NIL")) {
 						// Error info
+						System.out.println("ERROR: " + left.getValue() + " with Wrong Number of Arguments.");
+						System.exit(-1);
 					}
-					startEval(right.getLeft());
-					startEval(right.getRight());
+					leftArg = CADR(n);
+					rightArg = CADR(CDR(n));
+					startEval(leftArg);
+					startEval(rightArg);
 					int result;
-					if ((right.getLeft().getSubType()==0) && (right.getRight().getSubType()==0)) {
+					if ((leftArg.getSubType()==0) && (rightArg.getSubType()==0)) {
 						switch (left.getValue()):
-							case "PLUS": result = right.getLeft().getIntValue() + right.getRight().getIntValue();
-							case "MINUS": result = right.getLeft().getIntValue() - right.getRight().getIntValue();
-							case "TIMES": result = right.getLeft().getIntValue() * right.getRight().getIntValue();
-							case "QUOTIENT": result = right.getLeft().getIntValue() / right.getRight().getIntValue();
-							case "REMAINDER": result = right.getLeft().getIntValue() % right.getRight().getIntValue();
+							case "PLUS": result = leftArg.getIntValue() + irightArg.getIntValue();
+							case "MINUS": result = leftArg.getIntValue() - irightArg.getIntValue();
+							case "TIMES": result = leftArg.getIntValue() * irightArg.getIntValue();
+							case "QUOTIENT": result = leftArg.getIntValue() / irightArg.getIntValue();
+							case "REMAINDER": result = leftArg.getIntValue() % irightArg.getIntValue();
 					}
 					else {
 						// Error; Wrong Type;
-
+						System.out.println("ERROR: " + left.getValue() + " with Wrong Types of Arguments.");
+						System.exit(-1);
 					}
 					n.setValue(String.valueOf(result));
 					n.setLeft(null);
@@ -38,17 +67,25 @@ public class MyEvaluate {
 				}
 				else if ((left.getValue().equals("ATOM")) || (left.getValue().equals("INT")) || (left.getValue().equals("NULL"))) {
 					String result;
-					if (right==null) {
+					leftArg = CADR(n);
+					rightArg = CDDR(n);
+					if (rightArg.getValue.equals("NIL")) {
+						// Error info
+						System.out.println("ERROR: " + left.getValue() + " with Wrong Number of Arguments.");
+						System.exit(-1);
 					}
-					startEval(right);
-					if ((right.getType()==4) && (left.getValue().equals("ATOM"))) {
+					startEval(leftArg);
+					if ((leftArg.getType()==4) && (left.getValue().equals("ATOM"))) {
 						result="T";
 					}
-					else if ((right.getSubType()==0) && (left.getValue().equals("INT"))) {
+					else if ((leftArg.getSubType()==0) && (left.getValue().equals("INT"))) {
 						result="T";
 					}
-					else if ((right.getValue().equals("NIL")) && (left.getValue().equals("NULL"))) {
+					else if ((leftArg.getValue().equals("NIL")) && (left.getValue().equals("NULL"))) {
 						result="T";
+					}
+					else {
+						result="NIL";
 					}
 					n.setValue(result);
 					n.setLeft(null);
@@ -57,19 +94,26 @@ public class MyEvaluate {
 				}
 				else if ((left.getValue().equals("GREATER")) || (left.getValue().equals("LESS")) || (left.getValue().equals("EQUAL")))  {
 					String result;
-					if ((right.getLeft() == null) || (right.getRight() == null)) {
+					if (!CDDR(n).getValue().equals("NIL")) {
 						// Error info
+						System.out.println("ERROR: " + left.getValue() + " with Wrong Number of Arguments.");
+						System.exit(-1);
 					}
-					startEval(right.getLeft());
-					startEval(right.getRight());
-					if ((right.getLeft().getSubType()==0) && (right.getRight().getSubType()==0)) {
+					leftArg = CADR(n);
+					rightArg = CADR(CDR(n));
+					startEval(leftArg);
+					startEval(rightArg);
+
+					if ((leftArg.getSubType()==0) && (rightArg.getSubType()==0)) {
 						switch (left.getValue()):
-							case "GREATER": result = right.getLeft().getIntValue()>right.getRight().getIntValue()?T,NIL;
-							case "LESS": result = right.getLeft().getIntValue()<right.getRight().getIntValue()?T,NIL;
-							case "EQUAL": result = right.getLeft().getIntValue()==right.getRight().getIntValue()?T,NIL;
+							case "GREATER": result = leftArg.getIntValue()>rightArg.getIntValue()?T,NIL;
+							case "LESS": result = leftArg.getIntValue()<rightArg.getIntValue()?T,NIL;
+							case "EQUAL": result = leftArg.getIntValue()==rightArg.getIntValue()?T,NIL;
 					}
 					else {
-						// Error; Wrong Type;
+						// Error info
+						System.out.println("ERROR: " + left.getValue() + " with Wrong Number of Arguments.");
+						System.exit(-1);
 					}
 					n.setValue(result);
 					n.setLeft(null);
@@ -77,6 +121,7 @@ public class MyEvaluate {
 					n.setType(4);
 				}
 				else if (left.getValue().equals("CAR")) {
+
 					if ((right == 5) && (right.getLeft() != null)) {
 						n = right.getLeft();
 					}
@@ -131,7 +176,14 @@ public class MyEvaluate {
 					}
 				}
 				else if (left.getValue().equals("QUOTE")) {
-					n = n.getRight();
+					if (!CDDR(n).getValue().equals("NIL")) {
+						// Error info
+						System.out.println("ERROR: " + left.getValue() + " with Wrong Number of Arguments.");
+						System.exit(-1);
+					}
+					else {
+						n = CADR(n);
+					}
 				}
 		}
 		return true;
